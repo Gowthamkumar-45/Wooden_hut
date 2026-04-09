42c53f6a48c78fa26e2db64162bcca98b894b358
Gowtham <Gowthamtech11@gmail.com>
2026-04-09 15:25:21 +0530

    Database: Switch to permanent PostgreSQL storage for production

--- backend/Backend/settings.py
+++ backend/Backend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
 
 # BASE DIRECTORY
@@ -89,12 +90,12 @@ TEMPLATES = [
 WSGI_APPLICATION = 'Backend.wsgi.application'
 
 
-# DATABASE (SQLite for now)
+# DATABASE (SQLite for local, PostgreSQL for Render)
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600
+    )
 }
 
 
