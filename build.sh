046a7118d2e52c405c67895771c09298f20f51f9
Gowtham <Gowthamtech11@gmail.com>
2026-04-09 12:19:05 +0530

    Deployment: Auto-create admin account for Free Tier

--- build.sh
+++ build.sh
@@ -6,3 +6,6 @@ pip install -r backend/requirements.txt
 
 python backend/manage.py collectstatic --no-input
 python backend/manage.py migrate
+
+# Auto-create superuser for Free Tier
+python backend/manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='Gowtham').exists() or User.objects.create_superuser('Gowtham', 'Gowtham@gmail.com', 'Gowtham123')"
