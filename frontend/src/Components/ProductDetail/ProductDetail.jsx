569c17f3d54de54a8b04fccdc64cfd83a35f786d
Gowtham <Gowthamtech11@gmail.com>
2026-04-22 11:16:56 +0530

    Remove unused useNavigate import in ProductDetail.jsx

--- frontend/src/Components/ProductDetail/ProductDetail.jsx
+++ frontend/src/Components/ProductDetail/ProductDetail.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef, useCallback } from 'react';
-import { useParams, Link, useNavigate } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import { useForm, Controller } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
